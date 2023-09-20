cb051ef5c84bd40e55d1be36bfd44f00cbab2457
ivandjoh <idsta.ivandi@xl.co.id>
2023-09-20 23:34:49 +0700

    add ngrx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('./cakes/cakes.module').then(m => m.CakesModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/cakes/store/cakes.ts
@@ -0,0 +1,6 @@
+export interface Cakes {
+  id: number;
+  name: string;
+  description: string;
+  cost: number;
+}
